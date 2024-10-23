8305dcb99c87573d476084046c2a24f2457a6da1
masato <mm140701mm@gmail.com>
2024-10-23 23:59:37 +0900

    [add]自作エラーを追加

---
+++ src/utils/my_error.py
@@ -0,0 +1,16 @@
+# 自作エラークラス
+
+
+class ScrapeError(Exception):
+    pass
+
+
+class LessXXXXIDError(Exception):
+    pass
+
+
+class LessRaceinfoError(Exception):
+    pass
+
+class PreprocessError(Exception):
+    pass
